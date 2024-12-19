fa9a0064d7242973de12996969248c4fa47120de
capt-farvez <farvez.capt@gmail.com>
2024-12-20 05:53:40 +0600

    added static urls

--- DuoTalk/settings.py
+++ DuoTalk/settings.py
@@ -9,15 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8%tl4jezv49w7pdkgxjlv13%js3^vv92e-(uqso4co)y#xs$qa'
@@ -25,10 +21,8 @@ SECRET_KEY = 'django-insecure-8%tl4jezv49w7pdkgxjlv13%js3^vv92e-(uqso4co)y#xs$qa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -51,11 +45,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DuoTalk.urls'
-
-import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES = [
     {
@@ -138,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
